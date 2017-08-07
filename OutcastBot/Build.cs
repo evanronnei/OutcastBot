60b64e92a8ef4e6466dd0df74adb8249e12e88e4
Ronnei, Evan <eronnei@jjkeller.com>
2017-08-07 14:51:49 -0500

    added Build.cs

---
+++ OutcastBot/Build.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OutcastBot
+{
+    class Build
+    {
+        public DiscordUser Author { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ForumUrl { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}

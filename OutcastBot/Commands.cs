3d27305a4269fbf9693c8b2a484841f80d69d804
Ronnei, Evan <eronnei@jjkeller.com>
2017-08-07 14:05:37 -0500

    added initial !newbuild command

--- OutcastBot/Commands.cs
+++ OutcastBot/Commands.cs
@@ -3,6 +3,7 @@ using DSharpPlus.CommandsNext.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace OutcastBot
@@ -13,6 +14,17 @@ namespace OutcastBot
         public async Task Hi(CommandContext c)
         {
             await c.RespondAsync("sup");
+        }
+
+        [Command("newbuild")]
+        public async Task NewBuild(CommandContext c, string buildUrl)
+        {
+            var grimtoolsPattern = new Regex(@"http://www.grimtools.com/calc/(\w|\d){7}");
+
+            if (grimtoolsPattern.Match(buildUrl).Success)
+            {
+                await c.RespondAsync("valid grimtools url");
+            }
         }
     }
 }
